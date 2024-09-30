21afe7fec55a7c1cd0e9d8f6c4c971df13b8633b
NekoTRSH <konei2004@gmail.com>
2024-09-30 09:16:08 +0000

    firstcoomit

---
+++ simulation.c
@@ -0,0 +1,19 @@
+#include 	<stdio.h>
+#include	<stdlib.h>
+#include	<ctype.h>
+
+#define	RAM_CAPACITY	16
+#define VM_CAPACITY	32
+
+struct {
+	int processID;
+	int pageNum;
+	int lastAccessed;
+}
+
+int main(int argc, char *argv[]) {
+	
+	printf("Hello World!\n");
+
+	return 0;
+}
